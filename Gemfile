3a4f2ffc4a970e58b0f4dd58a5725261472dca1c
naoto.watanabe <miraoto@gmail.com>
2017-02-27 13:01:04 +0900

    add webpacker

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '2.4.0'
 
 gem 'rails', '5.1.0.beta1'
+gem 'webpacker', github: 'rails/webpacker'
 gem 'sprockets', '3.6.0'
 gem 'puma', '~> 3.0'
 gem 'slim-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -13,43 +13,43 @@
 ActiveRecord::Schema.define(version: 20170224011909) do
 
   create_table "actions", force: :cascade do |t|
-    t.string   "action_type",   null: false
-    t.string   "action_option"
-    t.string   "target_type"
-    t.integer  "target_id"
-    t.string   "user_type"
-    t.integer  "user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string "action_type", null: false
+    t.string "action_option"
+    t.string "target_type"
+    t.integer "target_id"
+    t.string "user_type"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["target_type", "target_id", "action_type"], name: "index_actions_on_target_type_and_target_id_and_action_type"
     t.index ["user_type", "user_id", "action_type"], name: "index_actions_on_user_type_and_user_id_and_action_type"
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer  "post_id"
-    t.integer  "user_id"
-    t.string   "body"
-    t.integer  "likes_count"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "post_id"
+    t.integer "user_id"
+    t.string "body"
+    t.integer "likes_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "user_id"
-    t.integer  "likes_count"
-    t.integer  "stars_count"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "title"
+    t.integer "user_id"
+    t.integer "likes_count"
+    t.integer "stars_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "followers_count"
-    t.integer  "following_count"
-    t.integer  "star_posts_count", default: 0
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.string "name"
+    t.integer "followers_count"
+    t.integer "following_count"
+    t.integer "star_posts_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
