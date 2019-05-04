958a2a4734171e20d964c321b7b779c63c1500a2
miraoto <miraoto@gmail.com>
2019-05-04 10:12:37 +0900

    Exec  and migrate

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,12 @@ Bundler.require(*Rails.groups)
 
 module InspectionApp
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
-    config.eager_load_paths += %W(#{config.root}/lib)
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
